701eac80e5654793272e34d087b49b6461b88322
lufamiko <ekosoneo12@gmail.com>
2023-11-29 20:01:52 +0700

    utils

---
+++ utils/formatIdr.js
@@ -0,0 +1,8 @@
+function formatIdr(rupiah) {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+    }).format(rupiah);
+}
+module.exports = formatIdr
